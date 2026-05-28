1d98d4e1e228bfe7551f15716baba391501d26b2
Rmxpvl <remy.pinville@gmail.com>
2026-05-28 11:44:19 +0200

    refactor convert_csv_to_json function to improve error handling and code structure

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,11 +4,15 @@ import csv
 import json
 
 def convert_csv_to_json(CSV_filename):
-    data = []
-    with open(CSV_filename, "r", encoding="utf-8") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
+    try:
+        data = []
+        with open(CSV_filename, "r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append(row)
 
-    with open("data.json", "w", encoding="utf-8") as file:
-        json.dump(data, file, indent=4)
+        with open("data.json", "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
+        return True
+    except (FileNotFoundError, OSError):
+        return False
