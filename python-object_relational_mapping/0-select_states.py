fb5b344f957ad2fd9e7c9fff7718aff038a767f1
Rmxpvl <remy.pinville@gmail.com>
2026-06-09 17:44:00 +0200

    fix: specify table name in SQL query for clarity

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     rows = cursor.fetchall()
 
