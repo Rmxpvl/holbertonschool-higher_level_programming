b5f13f998aca7e289706be583f024b629c75fa04
Rmxpvl <remy.pinville@gmail.com>
2026-06-12 18:50:29 +0200

    feat: add fetch_and_print_posts function to retrieve and display post titles

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,19 @@
 import requests
 import csv
 
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post["title"])
+
+
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
