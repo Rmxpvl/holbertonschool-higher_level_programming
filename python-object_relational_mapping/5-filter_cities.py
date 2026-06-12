92d3720a9d7b2e5ea30e66b64df96ab6d014551f
Rmxpvl <remy.pinville@gmail.com>
2026-06-12 13:44:05 +0200

    feat: add 5-filter_cities.py to list cities of a given state safely

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Lists all cities of a given state safely."""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+
+    cursor.execute(query, (argv[4],))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
