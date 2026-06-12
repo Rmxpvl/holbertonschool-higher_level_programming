b81ad85b2b10124666e17b4a06a7374b28c7fb47
Rmxpvl <remy.pinville@gmail.com>
2026-06-12 13:26:11 +0200

    fix: remove filtering by name from SQL query in 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,8 +16,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (argv[4],))
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     for row in cursor.fetchall():
         print(row)
