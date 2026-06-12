8f2d44b9e634b0e55623b26991a28a39142dc700
Rmxpvl <remy.pinville@gmail.com>
2026-06-12 18:48:22 +0200

    feat: add fetch_and_save_posts function to retrieve and save posts as CSV

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,25 @@
+import requests
+import csv
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # transformation en liste propre
+        posts = []
+
+        for post in data:
+            posts.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+
+        # écriture CSV
+        with open("posts.csv", "w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts)
