c67c86387d7b813312de55e875e9aca93d1687a6
Rmxpvl <remy.pinville@gmail.com>
2026-06-12 13:58:10 +0200

    fix: remove error handling and argument length check in 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,35 +5,27 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) != 5:
-        exit(1)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-        )
+    cursor = db.cursor()
 
-        cursor = db.cursor()
+    cursor.execute("""
+        SELECT cities.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id
+    """, (argv[4],))
 
-        query = """
-            SELECT cities.name
-            FROM cities
-            INNER JOIN states ON cities.state_id = states.id
-            WHERE states.name = %s
-            ORDER BY cities.id
-        """
+    cities = [city[0] for city in cursor.fetchall()]
+    if cities:
+        print(", ".join(cities))
 
-        cursor.execute(query, (argv[4],))
-
-        cities = [city[0] for city in cursor.fetchall()]
-        if cities:
-            print(", ".join(cities))
-
-        cursor.close()
-        db.close()
-    except MySQLdb.Error:
-        exit(1)
+    cursor.close()
+    db.close()
