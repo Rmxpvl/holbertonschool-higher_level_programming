1b7dccbbd8002bb742cf55399d93ea29b0096f89
Rmxpvl <remy.pinville@gmail.com>
2026-06-29 15:52:56 +0200

    Update-Add

---
+++ index.js
@@ -0,0 +1,42 @@
+// alert("Salut bg");
+
+let maSuperVariable = "Hello";
+
+// Les variables
+var Texte = "Salut je suis une variable";
+
+// Variable constantes
+const prenom = "Remy";
+
+// Let = Variable qui peux changer
+let monage = 34;
+monage = 45;
+let Profil = monage + prenom;
+
+let x = 7;
+let y = 15;
+
+if (x < y && x > 6) {
+  console.log("UI");
+}
+
+function LaBarreDeFaire() {
+  console.log("Je fais un calcul deuspi");
+  console.log(x + y);
+  alert("Calcul fini");
+}
+
+const addition = (a, b) => {
+  console.log(a + b);
+};
+
+addition(8, 9);
+LaBarreDeFaire();
+
+function add2() {
+  let num = 4;
+  console.log(num + 2);
+}
+
+console.log(num);
+
