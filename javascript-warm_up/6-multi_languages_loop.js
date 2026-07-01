2da09cb952abf66957b23c19c117db1b1489aa36
Rmxpvl <remy.pinville@gmail.com>
2026-07-01 14:38:02 +0200

    typo

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const textes = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const textes = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
     for (const texte of textes){
         console.log(texte);
