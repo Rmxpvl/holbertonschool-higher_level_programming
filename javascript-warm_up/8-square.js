bd96d3c88884227bb28bcdd38d2bd2cf2780f504
Rmxpvl <remy.pinville@gmail.com>
2026-07-01 15:34:22 +0200

    ''

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -6,10 +6,10 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    let line = "";
+    let line = '';
 
     for (let j = 0; j < size; j++) {
-      line += "X";
+      line += 'X';
     }
 
     console.log(line);
